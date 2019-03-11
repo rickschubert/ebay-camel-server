8dee817b529af81e81f9935f119c3edf58af8cbd
Rick Schubert <rickschubert@gmx.de>
2019-03-11 21:22:37 +0000

    Adding item to dynamodb (#1)

---
+++ README.md
@@ -0,0 +1,5 @@
+Go server writing to DynamoDB
+============================
+
+A simple REST web server which connects to DynamoDB (non-relational database hosted on AWS) and writes items to the database.
+
--- main.go
+++ main.go
@@ -5,22 +5,49 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/gorilla/mux"
+	"github.com/satori/go.uuid"
 )
 
-type articleToTrack struct {
+const awsDatabaseRegion = "eu-west-2"
+
+type itemToTrack struct {
 	SearchTerm string `json:"searchTerm"`
 	Price      int    `json:"price"`
+	UUID       string
 }
 
-func postyMcPost(w http.ResponseWriter, r *http.Request) {
-	var article articleToTrack
+type trackItemResponse struct {
+	Success bool   `json:"success"`
+	UUID    string `json:"uuid"`
+}
+
+func trackItem(w http.ResponseWriter, r *http.Request) {
+	var article itemToTrack
 	decoder := json.NewDecoder(r.Body)
 	decoder.Decode(&article)
+	article.UUID = uuid.Must(uuid.NewV4()).String()
 
-	fmt.Println(article)
+	av, err := dynamodbattribute.MarshalMap(article)
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("trackings"),
+	}
+	_, err = dynamoClient.PutItem(input)
+	if err != nil {
+		fmt.Println("An error occured when trying to post the item to DynamoDB:")
+		fmt.Println(err.Error())
+	}
 
-	js, err := json.Marshal(article)
+	response := trackItemResponse{
+		Success: true,
+		UUID:    article.UUID,
+	}
+	js, err := json.Marshal(response)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -31,8 +58,21 @@ func postyMcPost(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+var dynamoClient *dynamodb.DynamoDB
+
+func connectToDynamoDB() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(awsDatabaseRegion)},
+	)
+	if err != nil {
+		panic("Could not initiate new session with Dynamo DB.")
+	}
+	dynamoClient = dynamodb.New(sess)
+}
+
 func main() {
+	connectToDynamoDB()
 	router := mux.NewRouter()
-	router.HandleFunc("/api/track-product", postyMcPost).Methods("POST")
+	router.HandleFunc("/api/track-item", trackItem).Methods("POST")
 	http.ListenAndServe(":8079", router)
 }
